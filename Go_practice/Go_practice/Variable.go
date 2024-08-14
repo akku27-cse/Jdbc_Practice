bcab07caddeacd61e7191c2553f85d334ac3ce1d
Santu Jana <115920400+akku27-cse@users.noreply.github.com>
2024-08-14 14:03:48 +0530

    Add files via upload

---
+++ Go_practice/Go_practice/Variable.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt")
+
+func main(){
+	//decleatr variable
+	var name string="santu"
+      var i int=89;
+	fmt.Println(i)
+	  fmt.Println(name)
+
+	  var n,a string="santu","jana"
+	  fmt.Println(n,a)
+}
